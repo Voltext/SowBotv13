2e7eba6e5915c0f370e645d65e3cb147a848c309
Voltext <lokin84250@live.fr>
2021-12-23 16:17:01 +0100

    Ajout timeout

---
+++ Commands/Moderation/timeout.js
@@ -0,0 +1,42 @@
+const { CommandInteraction } = require("discord.js");
+const ms = require("ms")
+
+module.exports = {
+    name: "timeout",
+    description: "Permet a un utilisateur d'être bloqué des salons vocaux et textuels",
+    permission: "KICK_MEMBERS",
+    options: [
+        {
+            name: 'user',
+            description: "L'utilisateur à timeout",
+            type: "USER",
+            required: true
+        },
+        {
+            name: 'lenght',
+            description: "Durée du timeout",
+            type: "STRING",
+            required: true
+        },
+        {
+            name: 'reason',
+            description: "Raison du timeout",
+            type: "STRING",
+            required: true
+        }
+    ],
+
+    execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const lenght = interaction.options.getString('lenght');
+        const reason = interaction.options.getString('reason');
+        const member = interaction.options.getMember('user');
+
+        const timeIsMs = ms(lenght)
+        if(!timeIsMs)
+            return interaction.reply({ content: "Vous devez spécifier une durée valide : Exemple 10s pour 10 secondes", ephemeral: true });
+
+        member.timeout(timeIsMs, reason);
+        interaction.reply({ content: `Vous avez correctement timeout ${user} pour ${lenght} -> ${reason}`, ephemeral: true });
+    }
+}
