0a03cc30d7258825e7ce3a1480c7e0dc3d13307c
Voltext <lokin84250@live.fr>
2021-12-24 00:23:38 +0100

    Mise en place de la programmation automatique

---
+++ Api/twitchschedule.js
@@ -0,0 +1,24 @@
+const got = require('got');
+require('dotenv').config()
+
+//727375071
+const makeURL = () => `https://api.twitch.tv/helix/schedule?broadcaster_id=727375071`;
+
+class TwitchSchedule {
+    async Schedule() {
+        const res = await got(makeURL(), {
+             headers: {
+                 'Authorization': 'Bearer ' + process.env.TWITCH_AUTH,
+                 'Client-Id': process.env.CLIENT_ID
+             },
+             responseType: 'json'
+        })
+
+        if (!res || !res.body) {
+            throw new Error('Il y a une erreur dans la requête.')
+        }
+        return res.body;
+    }
+}
+
+module.exports = TwitchSchedule;
--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const TwitchLive = require("../../Api/twitchlive");
+const TwitchSchedule = require("../../Api/twitchschedule");
 const { MessageEmbed } = require("discord.js");
+const schedule = require('node-schedule');
 
 module.exports = {
     name: "ready",
@@ -50,6 +52,61 @@ module.exports = {
 					isOnLive=false;
 			}
 		}, 2 * 60 * 1000);
+
+		const getSchedule = new TwitchSchedule();
+		schedule.scheduleJob('0 0 * * *', async () => {
+
+			const programme = client.channels.cache.get(process.env.PROGRAMME);
+			programme.bulkDelete(1);
+
+			const prog = await getSchedule.Schedule()
+			if (prog.data.segments !== null) {
+				console.log(prog.data)
+				const programmation = prog.data.segments;
+				const embed = new MessageEmbed()
+					.setTitle("Programme de la semaine")
+					.setColor("PURPLE")
+					.setDescription("Voici le programme prévu pour la semaine")
+					.setThumbnail("https://static-cdn.jtvnw.net/jtv_user_pictures/191aab0a-e1ac-40c7-bfe5-e86a1257d598-profile_image-300x300.png")
+					.setURL("http://twitch.tv/sowdred");
+					programmation.forEach(function(elem) {
+						const date = elem.start_time.split("T")[0];
+						const heure = elem.start_time.split("T")[1];
+						embed.addFields({
+							name: "Date début",
+							value: `${date} ${heure}`,
+							inline: true,
+						}, {
+							name: "Titre",
+							value: `${elem.title}`,
+							inline: true,
+						}, {
+							name: "Catégorie",
+							value: `${elem.category.name}`,
+							inline: true,
+						});
+					})
+					client.channels.cache.get(process.env.PROGRAMME).send({
+						content: "Voici le programme de la semaine",
+						embeds: [embed],
+					});
+			}
+			else {
+				const embed = new MessageEmbed()
+					.setTitle("Programme de la semaine")
+					.setColor("PURPLE")
+					.setDescription("Voici le programme prévu pour la semaine")
+					.setThumbnail("https://static-cdn.jtvnw.net/jtv_user_pictures/191aab0a-e1ac-40c7-bfe5-e86a1257d598-profile_image-300x300.png")
+					.setURL("http://twitch.tv/sowdred")
+					.addField("Programme introuvable", "Le programme n'a pas encore été défini");
+					
+					client.channels.cache.get(process.env.PROGRAMME).send({
+						content: "Voici le programme de la semaine",
+						embeds: [embed],
+					});
+			}
+		})
+
 
         mongoose.connect(process.env.MONGO_PATH, {
             useNewUrlParser: true,
