89ef68241878517466d2cfa86a106efaa8c265cb
Voltext <lokin84250@live.fr>
2022-01-30 20:01:28 +0100

    Ajout suppression message log

---
+++ Events/Interaction/messageDelete.js
@@ -0,0 +1,24 @@
+const {
+    Client,
+    CommandInteraction,
+    MessageEmbed
+} = require("discord.js");
+
+module.exports = {
+    name: "messageDelete",
+    async execute(message) {
+        if(message.author.bot) return;
+
+        const logDelete = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`Un [message](${message.url}) a été supprimé par ${message.author} dans ${message.channel}.
+        **Message supprimé**: \`\`\`${message}\`\`\`
+        \n`)
+
+        if(message.attachments.size > 0) {
+            logDelete.addField(`Attachments:`, `${message.attachments.map((a) => a.url)}`, true)
+        }
+
+        message.guild.channels.cache.get(process.env.LOG_MESSAGE).send({embeds: [logDelete]})
+    }
+}
