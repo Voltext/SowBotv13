daa41a9066dff6c098cdaeb0dbcb5486f946b8a6
Voltext <lokin84250@live.fr>
2022-02-07 00:34:32 +0100

    Fix command

---
+++ Commands/Developpeur/say.js
@@ -0,0 +1,24 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "say",
+    description: "Permet de faire dire ce qu'on veut au bot",
+    permission: "ADMINISTRATOR",
+    options: [
+        {
+            name: 'phrase',
+            description: "La phrase a faire dire",
+            type: "STRING",
+            required: true
+        },
+    ],
+
+    execute(interaction) {
+        const { guild } = interaction;
+        const phrase = interaction.options.getString('phrase');
+        guild.channels.cache.get(process.env.GENERAL).send({
+            content: `${phrase}`
+        });
+        
+    }
+}
