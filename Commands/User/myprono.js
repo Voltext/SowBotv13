b69185ce839125f70e6e1e8de844a2ae2c8c161f
Voltext <fbricchi@esimed.fr>
2022-02-15 14:32:50 +0100

    fix

--- Commands/User/myprono.js
+++ Commands/User/myprono.js
@@ -1,38 +1,58 @@
-const { MessageEmbed } = require("discord.js");
+const {
+  MessageEmbed
+} = require("discord.js");
 const mongo = require('../../mongo');
 const rankPrediSchema = require('../../Schemas/rankPredictSchema')
 
 module.exports = {
-    name: "myprono",
-    description: "Permet a un utilisateur proposer un pronostique",
-    options: [
-      {
-          name: 'score',
-          description: "Score de l'utilisateur",
-          type: "STRING",
-          required: true
-      },
-    ],
+  name: "myprono",
+  description: "Permet a un utilisateur proposer un pronostique",
+  options: [{
+      name: "matchs",
+      description: "Les matchs dispos",
+      type: "STRING",
+      require: true,
+      choices: [{
+          name: "PSG - REAL",
+          value: "psg"
+        },
+        {
+          name: "OM - QARABAG",
+          value: "om"
+        },
+        {
+          name: "CHELSEA - LILLE",
+          value: "chelsea"
+        },
+      ]
+    },
+    {
+      name: 'score',
+      description: "Score de l'utilisateur",
+      type: "STRING",
+      required: true
+    },
+  ],
 
-    execute(interaction) {
-      const {
-        guild
+  execute(interaction) {
+    const {
+      guild
     } = interaction;
     const score = interaction.options.getString('score');
+    const match = options.getString('matchs')
 
     const embed = new MessageEmbed()
-    .setColor('DARKBLUE')
-    .setTitle(`${score}`)
-    .setAuthor(`${interaction.user.username}`)
-    .setDescription('Pour le match PSG - REAL');
+      .setColor('DARKBLUE')
+      .setTitle(`${score}`)
+      .setAuthor(`${match}`);
 
     guild.channels.cache.get(process.env.ADMIN_FEED).send({
       content: `Message de ${interaction.user.username}`,
       embeds: [embed],
-  })
-  interaction.reply({
-    content: 'Prédiction envoyé',
-    ephemeral: true,
-  })
-    }
+    })
+    interaction.reply({
+      content: 'Prédiction envoyé',
+      ephemeral: true,
+    })
+  }
 }
