61f75b0ece79deb2744eada14bc26d5b42cef185
Voltext <lokin84250@live.fr>
2022-02-21 20:59:42 +0100

    Fix temps

--- Commands/Game/event.js
+++ Commands/Game/event.js
@@ -43,20 +43,54 @@ module.exports = {
   async execute(interaction) {
     const {guild} = interaction;
     const equipe = interaction.options.getString("valeur")
+    let color = "";
+    let sec = "";
 
     const imgTeam = teamBDD[0][equipe][0]["image"];
     const nomTeam = teamBDD[0][equipe][0]["nom"];
     const difficulty = teamBDD[0][equipe][0]["difficulty"];
 
     const img = new MessageAttachment(imgTeam);
+    const time = 10000 * difficulty
+
+    if(difficulty === 1) {
+      color="GREEN";
+      sec= "10 secondes";
+    }
+    else if(difficulty === 2) {
+      color="ORANGE";
+      sec= "20 secondes";
+    }
+    else if(difficulty === 3) {
+      color="RED";
+      sec= "30 secondes";
+    }
 
     const embed = new MessageEmbed()
+    .setColor(color)
     .setTitle("Devinez l'équipe en fonction des nationalités des joueurs")
+    .setDescription(`Vous avez ${sec}`)
     .setImage(`attachment://${equipe}.png`);
 
-    guild.channels.cache.get(process.env.EVENT).send({
+    const filter = (m) => m.content === equipe
+
+    interaction.reply({
       embeds: [embed],
       files: [img]
+  })
+  .then(() => {
+    interaction.channel.awaitMessages({
+      filter,
+      max: 1,
+      time: time
+    })
+    .then(collected => {
+      const winner = collected.first().author.username
+      interaction.followUp(`Félicitations ${winner} ! `)
+    })
+    .catch(collected => {
+      interaction.followUp(`Personne n'a trouvé la solution dans le temps imparti `)
+    })
   })
   }
 }
