e065403c7f6708e5db9a1114ca4140a6f020d72b
Voltext <lokin84250@live.fr>
2022-02-24 03:13:23 +0100

    ajout joueur test

--- Commands/Game/event.js
+++ Commands/Game/event.js
@@ -4,7 +4,7 @@ const {
 } = require("discord.js");
 require('dotenv').config();
 const teamBDD = require("../../Game/Json/team.json")
-//const clubBDD = require("../../Game/Json/club.json")
+const clubBDD = require("../../Game/Json/club.json")
 //const goalStrikerBDD = require("../../Game/Json/goalstriker.json")
 
 function filtreTexte(arr, requete) {
@@ -50,7 +50,7 @@ module.exports = {
   }],
 
   async execute(interaction) {
-    const equipe = interaction.options.getString("valeur");
+    const value = interaction.options.getString("valeur");
     let color = "";
     let sec = "";
     let time = 0;
@@ -58,11 +58,11 @@ module.exports = {
 
     if (interaction.options.getString('type') === "drapeaux") {
 
-      if (teamBDD[0].hasOwnProperty(equipe)) {
-        const id = teamBDD[0][equipe][0]["id"];
-        const imgTeam = teamBDD[0][equipe][0]["image"];
-        const nomTeam = teamBDD[0][equipe][0]["nom"];
-        const difficulty = teamBDD[0][equipe][0]["difficulty"];
+      if (teamBDD[0].hasOwnProperty(value)) {
+        const id = teamBDD[0][value][0]["id"];
+        const imgTeam = teamBDD[0][value][0]["image"];
+        const nomTeam = teamBDD[0][value][0]["nom"];
+        const difficulty = teamBDD[0][value][0]["difficulty"];
 
         const img = new MessageAttachment(imgTeam);
 
@@ -112,12 +112,77 @@ module.exports = {
                 interaction.followUp(`Félicitations <@${winner}> ! `)
               })
               .catch(collected => {
-                interaction.followUp(`Personne n'a trouvé la solution dans le temps imparti. La bonne réponse était : ${equipe}`)
+                interaction.followUp(`Personne n'a trouvé la solution dans le temps imparti. La bonne réponse était : ${value}`)
               })
           })
       } else {
         interaction.reply({
           content: "L'équipe demandée n'existe pas",
+          ephemeral: true
+        })
+      }
+    }
+    if (interaction.options.getString('type') === "joueur") {
+      if (clubBDD[0].hasOwnProperty(value)) {
+        const id = clubBDD[0][value][0]["id"];
+        const imgJoueur = clubBDD[0][value][0]["image"];
+        const nomJoueur = clubBDD[0][value][0]["nom"];
+        const difficulty = clubBDD[0][value][0]["difficulty"];
+
+        const img = new MessageAttachment(imgJoueur);
+
+        if (difficulty === 1) {
+          libelle = "FACILE"
+          color = "GREEN";
+          sec = "30 secondes";
+          time = 30000
+        } else if (difficulty === 2) {
+          libelle = "MOYEN"
+          color = "ORANGE";
+          sec = "20 secondes";
+          time = 20000
+        } else if (difficulty === 3) {
+          libelle = "DIFFICILE"
+          color = "RED";
+          sec = "12 secondes";
+          time = 12000
+        } else if (difficulty === 4) {
+          libelle = "EXTREME"
+          color = "DARK_BUT_NOT_BLACK";
+          sec = "10 secondes";
+          time = 10000;
+        }
+
+        const embed = new MessageEmbed()
+          .setColor(color)
+          .setAuthor(`Difficulté : ${libelle}`)
+          .setTitle("Devinez l'équipe en fonction de la nationalité des joueurs")
+          .setDescription(`Vous avez ${sec}`)
+          .setImage(`attachment://${id}.png`);
+
+        const filter = (m) => nomJoueur.includes(m.content.toLowerCase())
+
+        interaction.reply({
+            embeds: [embed],
+            files: [img]
+          })
+          .then(() => {
+            interaction.channel.awaitMessages({
+                filter,
+                max: 1,
+                time: time
+              })
+              .then(collected => {
+                const winner = collected.first().author.id
+                interaction.followUp(`Félicitations <@${winner}> ! `)
+              })
+              .catch(collected => {
+                interaction.followUp(`Personne n'a trouvé la solution dans le temps imparti. La bonne réponse était : ${value}`)
+              })
+          })
+      } else {
+        interaction.reply({
+          content: "Le joueur demandé n'existe pas",
           ephemeral: true
         })
       }
