9efb9add6501916f9faa1995c9b85a713d41eb62
Voltext <lokin84250@live.fr>
2022-03-01 00:25:13 +0100

    fix

--- Events/Message/troll.js
+++ Events/Message/troll.js
@@ -23,8 +23,14 @@ const {
       } = message
       const messageContent = content.toLowerCase().split(" ");
   
-      if(messageContent[messageContent.length -1] === "quoi" || messageContent[messageContent.length -1] === "quoi?") {
-          message.channel.send("Feur ");
+      if(messageContent[messageContent.length -1] === "quoi" || messageContent[messageContent.length -1] === "quoi?" || messageContent[messageContent.length -1] === "Quoi?" || messageContent[messageContent.length -1] === "Quoi") {
+          message.channel.send("Feur...");
       }
+      if(messageContent[messageContent.length -1] === "hein?" || messageContent[messageContent.length -1] === "hein" || messageContent[messageContent.length -1] === "Hein" || messageContent[messageContent.length -1] === "Hein?") {
+        message.channel.send("2.");
+    }
+    if(messageContent[messageContent.length -1] === "Non" || messageContent[messageContent.length -1] === "non") {
+        message.channel.send("Bril...");
+    }
     }
   }
