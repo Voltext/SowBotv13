d46bebf0a239e03f7674373e5e00e3ea6ca21008
Voltext <fbricchi@esimed.fr>
2022-03-08 09:54:44 +0100

    revert

--- Systems/FilterSys.js
+++ Systems/FilterSys.js
@@ -1,17 +1,10 @@
 const Schema = require("../Schemas/filterSchema");
-const mongo = require('../mongo');
 
 module.exports = (client) => {
-  mongo().then(async (mongooseclient) => {
-    try {
-      Schema.find().then((documents) => {
-        documents.forEach((doc) => {
-          client.filters.set(doc.Guild, doc.Words);
-          client.filtersLog.set(doc.Guild, doc.Log);
-        });
-      });
-    } finally {
-      mongooseclient.connection.close()
-    }
-  })
+  Schema.find().then((documents) => {
+    documents.forEach((doc) => {
+      client.filters.set(doc.Guild, doc.Words);
+      client.filtersLog.set(doc.Guild, doc.Log);
+    });
+  });
 };
