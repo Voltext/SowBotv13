3584c4738c573b5d8b587fd7b3734a12ca0f9d06
Voltext <fbricchi@esimed.fr>
2022-03-08 09:52:33 +0100

    fix mongo error

--- Systems/FilterSys.js
+++ Systems/FilterSys.js
@@ -1,10 +1,17 @@
 const Schema = require("../Schemas/filterSchema");
+const mongo = require('../mongo');
 
 module.exports = (client) => {
-  Schema.find().then((documents) => {
-    documents.forEach((doc) => {
-      client.filters.set(doc.Guild, doc.Words);
-      client.filtersLog.set(doc.Guild, doc.Log);
-    });
-  });
+  mongo().then(async (mongooseclient) => {
+    try {
+      Schema.find().then((documents) => {
+        documents.forEach((doc) => {
+          client.filters.set(doc.Guild, doc.Words);
+          client.filtersLog.set(doc.Guild, doc.Log);
+        });
+      });
+    } finally {
+      mongooseclient.connection.close()
+    }
+  })
 };
