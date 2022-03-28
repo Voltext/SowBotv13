dcf2da873d7c4bb2b7db7e6c6d30947ee23a95fe
Voltext <lokin84250@live.fr>
2022-03-29 00:28:56 +0200

    fix

--- Commands/User/mycard.js
+++ Commands/User/mycard.js
@@ -12,16 +12,16 @@ module.exports = {
 
     execute(interaction) {
         const userId = interaction.user.id
+        let nbFiles = 0;
 
         const image = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}.png`))
 
         const attachment = new MessageAttachment(image)
 
-        const fs = require('fs');
         const dir = './Assets/Cards';
 
         fs.readdir(dir, (err, files) => {
-            const nbFiles = files.length
+            nbFiles = files.length
         });
 
         interaction.reply({
