50308bea7f9aa60ede55a1c538ab279d9762d5d1
Voltext <lokin84250@live.fr>
2022-03-30 19:33:01 +0200

    fix

--- Commands/User/mycard.js
+++ Commands/User/mycard.js
@@ -11,23 +11,24 @@ module.exports = {
     description: "Récupère ta carte",
 
     execute(interaction) {
-        const userId = interaction.user.id
-        let nbFiles = 0;
-        let attachmentBoost = ''
-
-        const image = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}.png`))
-
-        const attachment = new MessageAttachment(image)
-
-        const length = fs.readdirSync(path.join(__dirname, `../../Assets/Cards`)).length
-
-        interaction.reply({
-            content: `1/${length} cartes collectionnées`,
-            files: [attachment],
-            ephemeral: true
-        })
 
         try {
+            const userId = interaction.user.id
+            let nbFiles = 0;
+            let attachmentBoost = ''
+
+            const image = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}.png`))
+
+            const attachment = new MessageAttachment(image)
+
+            const length = fs.readdirSync(path.join(__dirname, `../../Assets/Cards`)).length
+
+            interaction.reply({
+                content: `1/${length} cartes collectionnées`,
+                files: [attachment],
+                ephemeral: true
+            })
+            
             const pathImg = `../../Assets/Cards/${userId}_boost.png`
             if (fs.existsSync(pathImg)) {
                 const imageBoost = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}_boost.png`))
@@ -45,6 +46,6 @@ module.exports = {
         } catch (err) {
             console.error(err)
         }
-        
+
     }
 }
