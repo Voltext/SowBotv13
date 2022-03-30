d205a28d44f24e4a50d46bb89b2c0f64d3b2cf6d
Voltext <lokin84250@live.fr>
2022-03-30 19:44:09 +0200

    console log debug

--- Commands/User/mycard.js
+++ Commands/User/mycard.js
@@ -27,8 +27,10 @@ module.exports = {
             ephemeral: true
         })
         try {
+            console.log("test")
             const pathImg = `../../Assets/Cards/${userId}_boost.png`
             if (fs.existsSync(pathImg)) {
+                console.log("test2")
                 const imageBoost = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}_boost.png`))
                 const attachmentBoost = new MessageAttachment(imageBoost)
                 interaction.followUp({
