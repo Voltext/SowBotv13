3956e1fbf97cd05462f5a744147bd715f201a798
Voltext <fbricchi@esimed.fr>
2022-04-04 11:48:10 +0200

    fix filtre

--- Systems/FilterSys.js
+++ Systems/FilterSys.js
@@ -1,10 +1,17 @@
 const Schema = require("../Schemas/filterSchema");
+const mongo = require('../../mongo');
 
 module.exports = (client) => {
-  Schema.find().then((documents) => {
-    documents.forEach((doc) => {
-      client.filters.set(doc.Guild, doc.Words);
-      client.filtersLog.set(doc.Guild, doc.Log);
-    });
+  await mongo().then(async (mongoosefiltre) => {
+    try {
+      await Schema.find().then((documents) => {
+        documents.forEach((doc) => {
+          client.filters.set(doc.Guild, doc.Words);
+          client.filtersLog.set(doc.Guild, doc.Log);
+        });
+      });
+    } finally {
+      mongoosefiltre.connection.close();
+    }
   });
 };
