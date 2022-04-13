b57c565d2addf06a8ea6b93d17e23f006e58b0a7
Voltext <fbricchi@esimed.fr>
2022-04-13 11:26:06 +0200

    fix

--- Events/Message/troll.js
+++ Events/Message/troll.js
@@ -18,22 +18,22 @@ module.exports = {
         const { content,guild,author,channel } = message
         const messageContent = content.toLowerCase().split(" ");
 
-        if (messageContent[messageContent.length - 1] === "quoi" || messageContent[messageContent.length - 1] === "quoi?" || messageContent[messageContent.length - 1] === "Quoi?" || messageContent[messageContent.length - 1] === "Quoi") {
-            message.channel.send("Feur...");
-        }
-        else if (messageContent[messageContent.length - 1] === "hein?" || messageContent[messageContent.length - 1] === "hein" || messageContent[messageContent.length - 1] === "Hein" || messageContent[messageContent.length - 1] === "Hein?") {
-                message.channel.send("2.");
-        }
-        else if (messageContent[messageContent.length - 1] === "Non" || messageContent[messageContent.length - 1] === "non") {
-            message.channel.send("Bril...");
-        }
-        else if (messageContent[messageContent.length - 1] === "Si" || messageContent[messageContent.length - 1] === "si") {
-            message.channel.send("Tron 🍋");
-        }
-        else if (messageContent[messageContent.length - 1] === "Oui" || messageContent[messageContent.length - 1] === "oui") {
-            message.channel.send("Stiti 🐒");
-        }
-        else if (messageContent[messageContent.length - 1] == "Karim" || messageContent[messageContent.length - 1] == "karim") {
+        // if (messageContent[messageContent.length - 1] === "quoi" || messageContent[messageContent.length - 1] === "quoi?" || messageContent[messageContent.length - 1] === "Quoi?" || messageContent[messageContent.length - 1] === "Quoi") {
+        //     message.channel.send("Feur...");
+        // }
+        // else if (messageContent[messageContent.length - 1] === "hein?" || messageContent[messageContent.length - 1] === "hein" || messageContent[messageContent.length - 1] === "Hein" || messageContent[messageContent.length - 1] === "Hein?") {
+        //         message.channel.send("2.");
+        // }
+        // else if (messageContent[messageContent.length - 1] === "Non" || messageContent[messageContent.length - 1] === "non") {
+        //     message.channel.send("Bril...");
+        // }
+        // else if (messageContent[messageContent.length - 1] === "Si" || messageContent[messageContent.length - 1] === "si") {
+        //     message.channel.send("Tron 🍋");
+        // }
+        // else if (messageContent[messageContent.length - 1] === "Oui" || messageContent[messageContent.length - 1] === "oui") {
+        //     message.channel.send("Stiti 🐒");
+        // }
+        if (messageContent[messageContent.length - 1] == "Kariiiiim" || messageContent[messageContent.length - 1] == "kariiiiim") {
             console.log("test");     
             const Embed = new MessageEmbed().setTitle("Kaaaaaariiiiim").setImage("attachment://goal.png")
             message.channel.send({
