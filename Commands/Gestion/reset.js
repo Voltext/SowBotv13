0a0a665ac284d2a3b0dcb435a094ba2caccae430
Voltext <fbricchi@esimed.fr>
2022-04-13 16:08:28 +0200

    fix

--- Commands/Gestion/reset.js
+++ Commands/Gestion/reset.js
@@ -2,26 +2,26 @@ const {
     CommandInteraction,
     MessageEmbed,
     MessageAttachment
-  } = require("discord.js");
+} = require("discord.js");
 require('dotenv').config();
 const mongo = require('../../mongo');
 const rankPrediSchema = require('../../Schemas/rankPredictSchema');
 const prediSchema = require('../../Schemas/prediSchema');
-  const {
+const {
     registerFont,
     createCanvas,
     loadImage
-  } = require("canvas")
-  const path = require('path');
-  registerFont('./Assets/Fonts/DINNextLTPro-Black.ttf', {
+} = require("canvas")
+const path = require('path');
+registerFont('./Assets/Fonts/DINNextLTPro-Black.ttf', {
     family: 'DINNextLTPro-Black'
-  })
-  registerFont('./Assets/Fonts/DINNextLTPro-UltraLightIt.ttf', {
+})
+registerFont('./Assets/Fonts/DINNextLTPro-UltraLightIt.ttf', {
     family: 'DINNextLTPro-UltraLightIt'
-  })
-  registerFont('./Assets/Fonts/DINNextRoundedLTPro-Bold.ttf', {
+})
+registerFont('./Assets/Fonts/DINNextRoundedLTPro-Bold.ttf', {
     family: 'DINNextRoundedLTPro-Bold'
-  })
+})
 
 module.exports = {
     name: "reset",
@@ -34,7 +34,9 @@ module.exports = {
         let pointsPlayer = '';
         let placement = 1;
 
-        const { guild } = interaction
+        const {
+            guild
+        } = interaction
 
         const month = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
 
@@ -63,8 +65,12 @@ module.exports = {
                     results.forEach(async function (elem) {
                         pseudos = pseudos + elem.userName + '\n';
                         pointsPlayer = pointsPlayer + elem.points + '\n';
-                        const member = guild.members.cache.get(elem.userId);
-                        await member.roles.add('936172336783171634');
+                        const members = await guild.members.fetch();
+                        members.forEach(async element => {
+                            if(element.user.id == elem.userId) {
+                                element.roles.add('936172336783171634')
+                            }
+                        })
                         battle.push([elem.userName, elem.points])
                         placement = placement + 1;
                     })
@@ -72,7 +78,7 @@ module.exports = {
                         name: '#',
                         value: "#",
                         inline: true
-                    },{
+                    }, {
                         name: '❯ Pseudo',
                         value: battle[0][0],
                         inline: true
@@ -86,64 +92,64 @@ module.exports = {
                 }
 
                 const canvas = createCanvas(1920, 1080)
-                    const ctx = canvas.getContext('2d')
-              
-                    const background = await loadImage(
-                      path.join(__dirname, `../../Assets/Base/Battle.png`)
-                    )
-                    let x = 0
-                    let y = 0
-                    ctx.drawImage(background, x, y)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name1 = `${battle[0][0]}`
-                    ctx.fillText(name1, 420, 280)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name2 = `${battle[7][0]}`
-                    ctx.fillText(name2, 420, 415)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name3 = `${battle[1][0]}`
-                    ctx.fillText(name3, 420, 660)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name4 = `${battle[6][0]}`
-                    ctx.fillText(name4, 420, 810)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name5 = `${battle[2][0]}`
-                    ctx.fillText(name5, 1280, 280)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name6 = `${battle[5][0]}`
-                    ctx.fillText(name6, 1280, 415)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name7 = `${battle[3][0]}`
-                    ctx.fillText(name7, 1280, 660)
-                
-                    ctx.fillStyle = '#000000'
-                    ctx.font = '30px DINNextLTPro-Black'
-                    let name8 = `${battle[4][0]}`
-                    ctx.fillText(name8, 1280, 810)
-              
-                    ctx.fillStyle = '#a69b94'
-                    ctx.font = '43px DINNextLTPro-Black'
-                    let month = `${name}`
-                    ctx.fillText(month.toUpperCase(), 1000, 250)
+                const ctx = canvas.getContext('2d')
 
-                    const attachment = new MessageAttachment(canvas.toBuffer())
+                const background = await loadImage(
+                    path.join(__dirname, `../../Assets/Base/Battle.png`)
+                )
+                let x = 0
+                let y = 0
+                ctx.drawImage(background, x, y)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name1 = `${battle[0][0]}`
+                ctx.fillText(name1, 420, 280)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name2 = `${battle[7][0]}`
+                ctx.fillText(name2, 420, 415)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name3 = `${battle[1][0]}`
+                ctx.fillText(name3, 420, 660)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name4 = `${battle[6][0]}`
+                ctx.fillText(name4, 420, 810)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name5 = `${battle[2][0]}`
+                ctx.fillText(name5, 1280, 280)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name6 = `${battle[5][0]}`
+                ctx.fillText(name6, 1280, 415)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name7 = `${battle[3][0]}`
+                ctx.fillText(name7, 1280, 660)
+
+                ctx.fillStyle = '#000000'
+                ctx.font = '30px DINNextLTPro-Black'
+                let name8 = `${battle[4][0]}`
+                ctx.fillText(name8, 1280, 810)
+
+                ctx.fillStyle = '#a69b94'
+                ctx.font = '43px DINNextLTPro-Black'
+                let month = `${name}`
+                ctx.fillText(month.toUpperCase(), 1000, 250)
+
+                const attachment = new MessageAttachment(canvas.toBuffer())
 
                 interaction.reply({
-                    embeds: [rankEmbed], 
+                    embeds: [rankEmbed],
                     files: [attachment]
                 })
                 // await mongo().then(async (mongoosereset2) => {
