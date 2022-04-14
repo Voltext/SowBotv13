3d414b0092bbe8f60f59dd76244c4001d0a9b0f2
Voltext <lokin84250@live.fr>
2022-04-14 15:31:39 +0200

    fix

--- Events/Message/FiltreDetection.js
+++ Events/Message/FiltreDetection.js
@@ -14,53 +14,53 @@ module.exports = {
    */
   async execute(message, client) {
     if (message.author.bot) return
-    if (message.channel.type === 'dm') return
+    if (!message.channel.type === 'dm') {
 
-    const {
-      content,
-      guild,
-      author,
-      channel
-    } = message
-    const messageContent = content.toLowerCase().split(" ");
+      const {
+        content,
+        guild,
+        author,
+        channel
+      } = message
+      const messageContent = content.toLowerCase().split(" ");
 
-    const Filter = client.filters.get(guild.id)
-    if (!Filter) return;
+      const Filter = client.filters.get(guild.id)
+      if (!Filter) return;
 
-    const wordsUsed = [];
-    let deleted = false;
+      const wordsUsed = [];
+      let deleted = false;
 
-    messageContent.forEach((word) => {
-      if (Filter.includes(word)) {
-        wordsUsed.push(word)
-        deleted = true
-      }
-    });
+      messageContent.forEach((word) => {
+        if (Filter.includes(word)) {
+          wordsUsed.push(word)
+          deleted = true
+        }
+      });
 
-    if (deleted) {
-      if (wordsUsed.length) {
-        message.delete();
-        const channelId = client.filtersLog.get(guild.id)
-        if (!channelId) return
-        const channelObject = guild.channels.cache.get(channelId)
-        if (!channelObject) return
+      if (deleted) {
+        if (wordsUsed.length) {
+          message.delete();
+          const channelId = client.filtersLog.get(guild.id)
+          if (!channelId) return
+          const channelObject = guild.channels.cache.get(channelId)
+          if (!channelObject) return
 
-        const Embed = new MessageEmbed()
-          .setColor("RED")
-          .setAuthor({
-            name: author.tag,
-            iconURL: author.displayAvatarURL({
-              dynamic: true
+          const Embed = new MessageEmbed()
+            .setColor("RED")
+            .setAuthor({
+              name: author.tag,
+              iconURL: author.displayAvatarURL({
+                dynamic: true
+              })
             })
-          })
-          .setDescription(
-            [
-              `A utilisé ${wordsUsed.length} mots interdits dans ${channel} =>`,
-              `\`${wordsUsed.map((w) => w)}\``,
-            ].join("\n")
-          );
-        const row = new MessageActionRow();
-        row.addComponents(
+            .setDescription(
+              [
+                `A utilisé ${wordsUsed.length} mots interdits dans ${channel} =>`,
+                `\`${wordsUsed.map((w) => w)}\``,
+              ].join("\n")
+            );
+          const row = new MessageActionRow();
+          row.addComponents(
             new MessageButton()
             .setCustomId('rien')
             .setLabel('Rien')
@@ -83,13 +83,14 @@ module.exports = {
             .setStyle('DANGER'),
           );
 
-        channelObject.send({
-          content: 'Nouveau mot interdit detecté',
-          embeds: [Embed],
-          components: [row]
-        })
-      }
-    };
+          channelObject.send({
+            content: 'Nouveau mot interdit detecté',
+            embeds: [Embed],
+            components: [row]
+          })
+        }
+      };
 
+    }
   }
 }
