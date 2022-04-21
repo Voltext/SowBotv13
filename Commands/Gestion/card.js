f682ff7bc28f92b376f44f43e5c4964c91988003
Voltext <lokin84250@live.fr>
2022-04-21 19:32:02 +0200

    Ajout selection d'une carte aléatoire

--- Commands/Gestion/card.js
+++ Commands/Gestion/card.js
@@ -1,36 +1,45 @@
-const { MessageEmbed } = require("discord.js");
+const {
+    MessageEmbed
+} = require("discord.js");
 const Cards = require("../../Api/card");
 require('dotenv').config();
+const fs = require('fs')
+const path = require('path');
 
 module.exports = {
-  name: "card",
-  description: "Affiche les nouvelles recuperations",
-  permission: "ADMINISTRATOR",
+    name: "card",
+    description: "Affiche les nouvelles recuperations",
+    permission: "ADMINISTRATOR",
 
-  async execute(interaction) {
-      const { guild } = interaction
+    async execute(interaction) {
+        const {
+            guild
+        } = interaction
 
-    const getUsers = new Cards()
+        const getUsers = new Cards()
 
-    const card = await getUsers.getUserCard()
-    if (card.data !== null) {
-        const data = card.data
+        const card = await getUsers.getUserCard()
+        if (card.data !== null) {
+            const data = card.data
 
-        data.forEach(function (elem) {
-            const embed = new MessageEmbed()
-            .setTitle("Nouvelle demande de carte")
-            .setDescription(`${elem.user_name} vient de récupérer une carte ! Félicitations`)
+            data.forEach(function (elem) {
+                const embed = new MessageEmbed()
+                    .setTitle("Nouvelle demande de carte")
+                    .setDescription(`${elem.user_name} vient de récupérer une carte ! Félicitations`)
+                
+                var files = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/`))
+                let chosenFile = files[Math.floor(Math.random() * files.length)]
 
-            guild.channels.cache.get(process.env.ADMIN_FEED).send({
-                embeds: [embed]
+                guild.channels.cache.get(process.env.ADMIN_FEED).send({
+                    content: chosenFile,
+                    embeds: [embed]
+                })
             })
-        })
 
+        } else {
+            interaction.reply({
+                content: "Aucune demande de carte n'a été faites récemment"
+            })
+        }
     }
-    else {
-        interaction.reply({
-            content: "Aucune demande de carte n'a été faites récemment"
-        })
-    }
-  }
 }
