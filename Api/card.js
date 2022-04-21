a69e21f76403d767f539ba1d87858790fa3b122a
Voltext <lokin84250@live.fr>
2022-04-21 19:07:05 +0200

    ajout commande carte

---
+++ Api/card.js
@@ -0,0 +1,23 @@
+const got = require('got');
+require('dotenv').config()
+
+const makeURL = () => `https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions?broadcaster_id=727375071&reward_id=dd830257-d211-41fa-9c41-89472c032a9f&status=FULFILLED&sort=NEWEST`;
+
+class Card {
+    async getUserCard() {
+        const res = await got(makeURL(), {
+             headers: {
+                 'Authorization': 'Bearer ' + process.env.TOKEN_SOW,
+                 'Client-Id': process.env.CLIENT_ID_SOW
+             },
+             responseType: 'json'
+        })
+
+        if (!res || !res.body) {
+            throw new Error('Il y a une erreur dans la requête.')
+        }
+        return res.body;
+    }
+}
+
+module.exports = Card;
---
+++ Commands/Gestion/card.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require("discord.js");
+const Cards = require("../../Api/card");
+require('dotenv').config();
+
+module.exports = {
+  name: "card",
+  description: "Affiche les nouvelles recuperations",
+  permission: "ADMINISTRATOR",
+
+  async execute(interaction) {
+
+    const getUsers = new Cards()
+
+    const card = await getUsers.getUserCard()
+    if (card.data !== null) {
+        const data = card.data
+
+        data.forEach(function (elem) {
+            const embed = new MessageEmbed()
+            .setTitle("Nouvelle demande de carte")
+            .setDescription(`${elem.user_name} vient de récupérer une carte ! Félicitations`)
+
+            interaction.reply({
+                embeds: [embed]
+            })
+        })
+
+    }
+    else {
+        interaction.reply({
+            content: "Aucune demande de carte n'a été faites récemment"
+        })
+    }
+  }
+}
