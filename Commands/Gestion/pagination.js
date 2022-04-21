e8ca222f0f2567e4c94e57d9664b4d4b01816912
Voltext <lokin84250@live.fr>
2022-04-21 22:39:20 +0200

    fix

--- Commands/Gestion/pagination.js
+++ Commands/Gestion/pagination.js
@@ -44,6 +44,6 @@ module.exports = {
                 mongoosepredi.connection.close();
             }
         })  
-        pagination(interaction, ArrEmb)
+        pagination(interaction, ArrEmb[0])
     }
 }
