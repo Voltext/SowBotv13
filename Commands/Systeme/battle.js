fcaef79325c3f637654d9744d77fa74f2eb5a520
Voltext <lokin84250@live.fr>
2022-04-23 14:19:54 +0200

    fix typo

--- Commands/Systeme/battle.js
+++ Commands/Systeme/battle.js
@@ -218,7 +218,7 @@ module.exports = {
                                     // for each element
                                   ])
                                     .then((result) => {
-                                      console.log(result.prono_responses);
+                                      console.log(result.prono_reponses);
                                     })
                                     .catch((error) => {
                                       console.log(error);
