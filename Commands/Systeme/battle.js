737e82f73198475e09ba9ed71494597081504e45
Voltext <lokin84250@live.fr>
2022-04-23 13:26:15 +0200

    debug

--- Commands/Systeme/battle.js
+++ Commands/Systeme/battle.js
@@ -225,7 +225,7 @@ module.exports = {
                                 } */
 
 
-                            } finally {
+                            } catch {
                                 mongooserank.connection.close();
                             }
                         })
