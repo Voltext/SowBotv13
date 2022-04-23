ccef49934b12248d8a256f69b44a4c38e6e4530e
Voltext <lokin84250@live.fr>
2022-04-23 15:25:14 +0200

    debug

--- Commands/Systeme/battle.js
+++ Commands/Systeme/battle.js
@@ -258,6 +258,7 @@ module.exports = {
                                     }
                                 })
                             })
+                            console.log(players)
                         }
 
 
