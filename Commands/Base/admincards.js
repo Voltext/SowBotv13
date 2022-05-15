1b444830d109cfa35aec85aca33f585b08fab040
Voltext <lokin84250@live.fr>
2022-05-15 23:44:34 +0200

    fix

--- Commands/Base/admincards.js
+++ Commands/Base/admincards.js
@@ -20,35 +20,8 @@ module.exports = {
         const pagination = new Pagination(interaction);
 
         const userId = interaction.user.id
-        const userName = interaction.user.username
         var ArrEmb = []
         var ArrImg = []
-
-        const imageBasic = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}.png`))
-
-        const attachmentBasic = new MessageAttachment(imageBasic)
-        const embed = new MessageEmbed().setImage(`attachment://${userId}`)
-
-        const length = fs.readdirSync(path.join(__dirname, `../../Assets/Cards`)).length
-
-        let nbCards = 0
-
-        ArrImg.push(attachmentBasic)
-        ArrEmb.push(embed)
-
-        try {
-            const pathImg = path.join(__dirname, `../../Assets/Cards/${userId}_boost.png`)
-            if (fs.existsSync(pathImg)) {
-                const imageBoost = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}_boost.png`))
-                const attachmentBoost = new MessageAttachment(imageBoost)
-                const embeds = new MessageEmbed().setImage(`attachment://${userId}`)
-
-                ArrImg.push(attachmentBoost)
-                ArrEmb.push(embeds)
-            }
-        } catch (err) {
-            console.error(err)
-        }
 
         await mongo().then(async (mongoosepredi) => {
             try {
