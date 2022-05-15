af30343838ab33c377d4c952827bbdeb3bcc892a
Voltext <lokin84250@live.fr>
2022-05-15 23:52:27 +0200

    fix

--- Commands/User/mycard.js
+++ Commands/User/mycard.js
@@ -16,35 +16,7 @@ module.exports = {
     async execute(interaction) {
 
         const userId = interaction.user.id
-        const userName = interaction.user.username
         var ArrEmb = []
-        var ArrImg = []
-
-        const imageBasic = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}.png`))
-
-        const attachmentBasic = new MessageAttachment(imageBasic)
-        const embed = new MessageEmbed().setImage(`attachment://${userId}`)
-
-        const length = fs.readdirSync(path.join(__dirname, `../../Assets/Cards`)).length
-
-        let nbCards = 0
-
-        ArrImg.push(attachmentBasic)
-        ArrEmb.push(embed)
-
-        try {
-            const pathImg = path.join(__dirname, `../../Assets/Cards/${userId}_boost.png`)
-            if (fs.existsSync(pathImg)) {
-                const imageBoost = fs.readFileSync(path.join(__dirname, `../../Assets/Cards/${userId}_boost.png`))
-                const attachmentBoost = new MessageAttachment(imageBoost)
-                const embeds = new MessageEmbed().setImage(`attachment://${userId}`)
-                
-                ArrImg.push(attachmentBoost)
-                ArrEmb.push(embeds)
-            }
-        } catch (err) {
-            console.error(err)
-        }
 
         await mongo().then(async (mongoosepredi) => {
             try {
@@ -55,6 +27,10 @@ module.exports = {
                     
                 } else {
                     const ArrCards = results.cards
+
+                    const embed = new MessageEmbed().setImage(`http://141.94.78.72/Cards/${userId}.png`)
+                        
+                    ArrEmb.push(embed)
 
                     ArrCards.forEach(function (elem) {
                         const embed = new MessageEmbed().setImage(`http://141.94.78.72/Cards/${elem}`)
