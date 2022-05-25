0b073d4f09eb46a577b86479b94cbfe5109ac6f7
Voltext <fbricchi@esimed.fr>
2022-05-25 16:13:42 +0200

    Fix modal response

--- Events/Modal/deleteTeamModal.js
+++ Events/Modal/deleteTeamModal.js
@@ -1,5 +1,5 @@
 const { Modal } = require('discord-modals')
-const { Formatters } = require('discord.js');
+const { Formatters, MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: "modalSubmit",
@@ -13,8 +13,9 @@ module.exports = {
     if(modal.customId === 'modal-customid') {
       const firstResponse = modal.getTextInputValue('textinput-customid');
       modal.member.guild.channels.cache.get(process.env.ADMIN_FEED).send({
-        content: `${modal.user.username} vient de supprimer son équipe pour la raison suivante : ${Formatters.codeBlock('markdown', firstResponse)}`
+        embeds: [new MessageEmbed().setTitle("Suppressiond 'équipe").setDescription(`${modal.user.username} vient de supprimer son équipe pour la raison suivante : ${Formatters.codeBlock('markdown', firstResponse)}`).setColor('RED')]
       })
+      modal.reply({ content: 'La suppression de votre équipe a bien été prise en compte pour la raison : ' + Formatters.codeBlock('markdown', firstResponse), ephemeral: true });
     } 
 
     
