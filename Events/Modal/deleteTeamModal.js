8a23e3b316ad75ac5f442e90e42f076b6b5987e9
Voltext <fbricchi@esimed.fr>
2022-05-25 16:16:20 +0200

    fix typo

--- Events/Modal/deleteTeamModal.js
+++ Events/Modal/deleteTeamModal.js
@@ -13,9 +13,9 @@ module.exports = {
     if(modal.customId === 'modal-customid') {
       const firstResponse = modal.getTextInputValue('textinput-customid');
       modal.member.guild.channels.cache.get(process.env.ADMIN_FEED).send({
-        embeds: [new MessageEmbed().setTitle("Suppressiond 'équipe").setDescription(`${modal.user.username} vient de supprimer son équipe pour la raison suivante : ${Formatters.codeBlock('markdown', firstResponse)}`).setColor('RED')]
+        embeds: [new MessageEmbed().setTitle("Suppression d'équipe").setDescription(`${modal.user.username} vient de supprimer son équipe pour la raison suivante : ${Formatters.codeBlock('markdown', firstResponse)}`).setColor('RED')]
       })
-      
+
       modal.reply({ content: `La suppression de votre équipe a bien été prise en compte pour la raison : ${Formatters.codeBlock('markdown', firstResponse)}`, ephemeral: true });
     } 
 
