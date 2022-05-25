e7c2a127ba7cbed05b522d6780bbb3924264ca11
Voltext <fbricchi@esimed.fr>
2022-05-25 15:57:11 +0200

    fix reponse modal

--- Events/Modal/deleteTeamModal.js
+++ Events/Modal/deleteTeamModal.js
@@ -1,4 +1,5 @@
 const { Modal } = require('discord-modals')
+const { Formatters } = require('discord.js');
 
 module.exports = {
   name: "modalSubmit",
@@ -8,10 +9,10 @@ module.exports = {
   */
 
   async execute(modal) {
-    if(modal.customId !== 'teamModal') return
-
-    const value = modal.getTextInputValue('raisonRemove')
-    console.log(modal)
+    if(modal.customId === 'teamModal') {
+      const firstResponse = modal.getTextInputValue('textinput-customid');
+      modal.reply('Congrats! Powered by discord-modals.' + Formatters.codeBlock('markdown', firstResponse));
+    } 
 
     guild.channels.cache.get(process.env.ADMIN_FEED).send({
       content: `vient de supprimer son équipe pour la raison : ${value}`
