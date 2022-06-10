d3be24b5ac7883118217f67e97d4248276606ae9
Voltext <lokin84250@live.fr>
2022-06-10 02:58:10 +0200

    fix

--- Commands/Developpeur/sondage.js
+++ Commands/Developpeur/sondage.js
@@ -52,7 +52,7 @@ module.exports = {
       content: `Sondage lancé : Quel serait pour vous la recrue idéale pour : ${equipe} ?`
     })
 
-    const filter = m => i.user.id !== "907244439037169685"
+    const filter = m => m.user.id !== "907244439037169685"
 
     const collector = interaction.channel.createMessageCollector({
       filter,
