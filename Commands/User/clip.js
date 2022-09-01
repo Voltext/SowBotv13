7d5f8c851f2264d3e24151ba13c7df4575ee7310
Voltext <lokin84250@live.fr>
2022-09-01 23:54:17 +0200

    ajout commande clip

---
+++ Commands/User/clip.js
@@ -0,0 +1,42 @@
+const mongo = require('../../mongo');
+const linkTwitchSchema = require('../../Schemas/linkTwitchSchema')
+
+module.exports = {
+  name: "clip",
+  description: "Envoyer son clip pour la communauté",
+  options: [{
+    name: 'lien',
+    description: "Lien du clip",
+    type: "STRING",
+    required: true
+  }, ],
+
+  async execute(interaction) {
+
+    const { guild } = interaction
+
+    const lien = interaction.options.getString("lien")
+    const clipChannelAdmin = guild.channels.cache.get(
+			process.env.CLIPADMIN_FEED,
+		);
+    
+    const embedClip = new Discord.MessageEmbed()
+					.setColor('BLUE')
+					.setTitle(`Nouveau clip !`)
+					.setDescription('Utilisez les réactions pour valider ou non ce clip')
+					.setURL(lien);
+
+				clipChannelAdmin.send(embedClip).then(message => {
+					message.react(process.env.CHECK_ID);
+					message.react(process.env.CROSS_ID);
+				});
+
+  interaction.reply({
+      content: `Le clip a bien été envoyé, il sera soumis à la validation des modérateurs`,
+      ephemeral: true
+  })
+    
+
+
+  }
+}
