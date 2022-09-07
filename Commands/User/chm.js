820ded374e14a8937ecf5192f9a2f6e1e2e90782
Voltext <fbricchi@esimed.fr>
2022-09-07 10:18:20 +0200

    fix

--- Commands/User/chm.js
+++ Commands/User/chm.js
@@ -188,7 +188,14 @@ module.exports = {
               const attachement = new MessageAttachment(image, "graph.png")
 
               interaction.reply({
-                embeds: [new MessageEmbed().setTitle("Test").setThumbnail(userObj.profil).setImage("attachment://graph.png")],
+                embeds: [new MessageEmbed().setTitle("Vos statistiques").setThumbnail(userObj.profil).setImage("attachment://graph.png").addFields(
+                  { name: keysChart[0], value: userObj.stat1, inline: true },
+                  { name: keysChart[1], value: userObj.stat2, inline: true },
+                  { name: keysChart[2], value: userObj.stat3, inline: true },
+                  { name: keysChart[3], value: userObj.stat4, inline: true },
+                  { name: keysChart[4], value: userObj.stat5, inline: true },
+                  { name: keysChart[5], value: userObj.stat6, inline: true },
+                )],
                 files: [attachement],
                 ephemeral: true,
               })
