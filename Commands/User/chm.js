12d82d0716ed17c063f6e56ae487f67d851069fe
Voltext <fbricchi@esimed.fr>
2022-09-13 17:48:13 +0200

    Restrict to moderators

--- Commands/User/chm.js
+++ Commands/User/chm.js
@@ -25,7 +25,7 @@ const progressbar = require('string-progressbar');
 module.exports = {
   name: "chm",
   description: "Club house manager",
-  permission: "SEND_MESSAGES",
+  permission: "KICK_MEMBERS",
   options: [{
       name: "myplayer",
       type: "SUB_COMMAND",
