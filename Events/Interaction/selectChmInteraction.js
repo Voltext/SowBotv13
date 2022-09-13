d98b2adad927e78f29a14b73270d51849934e5f8
Voltext <fbricchi@esimed.fr>
2022-09-13 17:34:53 +0200

    debug

--- Events/Interaction/selectChmInteraction.js
+++ Events/Interaction/selectChmInteraction.js
@@ -4,14 +4,15 @@ const {
 
 module.exports = {
   name: "interactionCreate",
-  async execute(interaction, client) {
+  async execute(interaction) {
+    console.log(interaction)
     if (!interaction.isSelectMenu()) return;
 
     if (interaction.customId === 'select') {
       interaction.update({ content: 'Entrainement en cours...', components: [] })
       .then(()=> {
         setTimeout(function(){
-          interaction.followUp({ content: 'Entrainement terminé !', components: [] });
+          interaction.followUp({ content: 'Entrainement terminé !', components: [], ephemeral: true });
         }, 5000)
       }); ;
     }
