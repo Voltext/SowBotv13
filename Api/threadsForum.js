8f5660f877b700ab94e2f7e9a59950f31822ead6
Voltext <fbricchi@esimed.fr>
2022-09-16 12:14:34 +0200

    debug

--- Api/threadsForum.js
+++ Api/threadsForum.js
@@ -5,21 +5,23 @@ const makeURL = () => `https://discord.com/api/channels/1020265346877374534/thre
 
 class Thread {
     async insertThreadForum(message, name) {
-        const res = await got.post(makeURL(), {
-             headers: {
-                 'Authorization': 'Bot ' + process.env.BOT_TOKEN,
-                 'Content-Type': "application/json"
-             },
-             body: {
-              message : {
-                  content: message
-              },
-              name: name
+      requestOptions = [
+        {
+          headers: {
+            'Authorization': 'Bot ' + process.env.BOT_TOKEN,
+            'Content-Type': "application/json"
           },
-          json: true,
-          form: true,
-          responseType: 'json'
-        })
+            method: "POST",
+            form: true,
+            body: {
+              message : {
+                content: message
+            },
+            name: name
+            }
+        }
+    ];
+        const res = await got(makeURL(), requestOptions)
 
         if (!res || !res.body) {
             throw new Error('Il y a une erreur dans la requête.')
