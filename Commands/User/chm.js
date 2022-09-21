1cccd860e85ec783ed3c22a1e47dba7050bbb88e
Voltext <fbricchi@esimed.fr>
2022-09-21 09:54:03 +0200

    fix

--- Commands/User/chm.js
+++ Commands/User/chm.js
@@ -816,7 +816,7 @@ module.exports = {
                           });
                           headers = {
                             'Authorization': 'Bot ' + process.env.BOT_TOKEN,
-                            'Content-Type': 'application/json'
+                            'Content-Type': 'multipart/form-data'
                           }
 
                           dataCards = {
