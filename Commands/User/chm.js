20f90fd4f7b0e4ba1dc17d9cc7fd22b217877df3
Voltext <fbricchi@esimed.fr>
2022-09-21 13:44:40 +0200

    fix

--- Commands/User/chm.js
+++ Commands/User/chm.js
@@ -833,8 +833,10 @@ module.exports = {
                             "name": `[NOUVEAU TRANSFERT] ${userObj.joueurName} a rejoint ${userObjTeamPlayer.team.teamName} pour ${userObj.montant}`
                           }
 
+                          const image = fs.readFile("../../Assets/ekipe.png");
+
                           var formData = new FormData();
-                          formData.append('files[0]', "../../Assets/ekipe.png", "file.png");
+                          formData.append('files[0]', image, "file.png");
                           formData.append('payload_json', JSON.stringify(dataCards));
 
                           axios({
