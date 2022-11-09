7bbc9a76316559e9e897bda0d8cf885db645c7b6
Voltext <lokin84250@live.fr>
2022-11-09 14:17:15 +0100

    debug

--- Events/Button/AbsenceInteraction.js
+++ Events/Button/AbsenceInteraction.js
@@ -10,7 +10,8 @@ module.exports = {
         const {guild} = interaction
         if (interaction.isButton()) {
            if(interaction.customId === 'valideAbsence') {
-            const userId = interaction.message.embeds.footer
+            console.log(interaction.message.embeds.footer)
+            const userId = interaction.message.embeds.footer.text
             const etat = "Validée"
             await mongo().then(async (mongoosenewabsence) => {
                 try {
