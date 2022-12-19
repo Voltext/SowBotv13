2f8493e70e6eccb0eaa53716b28ef172f45445ea
Voltext <lokin84250@live.fr>
2022-12-19 20:51:23 +0100

    ix

--- Schemas/mysql/PlayerMysql.js
+++ Schemas/mysql/PlayerMysql.js
@@ -1,9 +1,9 @@
 const Util = require("../../Utils/function")
-const {connection} = require("./Connect")
+const bdd = require("./Connect")
 
 module.exports = class PlayerMysql {
     static getPlayer(userId) {
-        connection.execute(
+        bdd.connection.query(
             `SELECT * FROM players WHERE userId = ${userId}`,
             function(err, results, fields) {
                 return results
