70284d73267384b07e341512bc24c136a4330a66
Voltext <lokin84250@live.fr>
2022-12-19 20:54:19 +0100

    fix

--- Schemas/mysql/PlayerMysql.js
+++ Schemas/mysql/PlayerMysql.js
@@ -1,8 +1,8 @@
-const Util = require("../../Utils/function")
 const bdd = require("./Connect")
 
 module.exports = class PlayerMysql {
     static getPlayer(userId) {
+        console.log(bdd);
         bdd.connection.query(
             `SELECT * FROM players WHERE userId = ${userId}`,
             function(err, results, fields) {
--- Utils/function.js
+++ Utils/function.js
@@ -73,19 +73,6 @@ module.exports = class Utils {
         return milliseconds;
     }
 
-    static getBDD() {
-        const connection = mysql.createConnection({
-            host: process.env.HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-          });
-          connection.connect(err => {
-			if(err) throw err;
-		});
-		return connection;
-    }
-
     static addStat(userId, stat, point, stamina, userObj) {
 
         let update = {}
