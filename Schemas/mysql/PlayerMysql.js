a67e244c1a38239f709c14ba868256507108cdcb
Voltext <lokin84250@live.fr>
2022-12-19 21:10:24 +0100

    fix

--- Schemas/mysql/PlayerMysql.js
+++ Schemas/mysql/PlayerMysql.js
@@ -3,9 +3,8 @@ const bdd = Util.getBDD();
 
 module.exports = class PlayerMysql {
     static getPlayer(userId) {
-        console.log(bdd)
         bdd.query(
-            `SELECT * FROM players WHERE userId = ${userId}`,
+            `SELECT * FROM players WHERE userId = '${userId}'`,
             function(err, results, fields) {
                 return results
             }
