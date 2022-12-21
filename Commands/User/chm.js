42b13d9b4d5168e28de848158cff9c9a2f70614a
Voltext <fbricchi@esimed.fr>
2022-12-21 11:48:08 +0100

    fix

--- Commands/User/chm.js
+++ Commands/User/chm.js
@@ -221,6 +221,7 @@ module.exports = {
           backgroundColour: 'white'
         })
         const playerData = await PlayerMysql.getPlayer(userId)
+        console.log(playerData)
             if (playerData[0] !== "") {
               var total = 100;
               var current = playerData[0].stamina;
