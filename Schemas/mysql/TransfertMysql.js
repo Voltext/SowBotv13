e5d9f8d5677e88ed53bf7d458b5e354f79a9bb9d
Voltext <lokin84250@live.fr>
2022-12-22 14:50:22 +0100

    fix

--- Schemas/mysql/TransfertMysql.js
+++ Schemas/mysql/TransfertMysql.js
@@ -6,4 +6,9 @@ module.exports = class TransfertMysql {
         return data
     }
 
+    static async insertTransfertLibre(joueur, to, montant, thread, statut) {
+        const [data] = await db.execute(`INSERT INTO transferts (joueurId, toTeamId, montant, threadId, status) VALUES ('${joueur}', '${to}', '${montant}', '${thread}', '${statut}')`);
+        return data
+    }
+
 }
