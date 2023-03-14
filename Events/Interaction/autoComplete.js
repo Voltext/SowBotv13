c09d31c0bdd3ef92193d72be52f189ca8be387d9
Voltext <lokin84250@live.fr>
2023-03-14 21:00:13 +0100

    fix

--- Events/Interaction/autoComplete.js
+++ Events/Interaction/autoComplete.js
@@ -13,7 +13,7 @@ module.exports = {
         const filtered = propertyNames.filter(choice => choice.Surname.startsWith(entry));
         await interaction.respond(
           filtered.slice(0, 25).map(choice => ({
-            name: choice.FirstName + choice.Surname,
+            name: choice.Forename + choice.Surname,
             value: choice.Surname
           })),
         );
