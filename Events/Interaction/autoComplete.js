0091cd6923c2d23303e5de0c541e51c6b80e5280
Voltext <fbricchi@esimed.fr>
2023-03-14 17:24:06 +0100

    test autocompletion

---
+++ Events/Interaction/autoComplete.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "interactionCreate",
+  async execute(interaction, client) {
+      if(interaction.isAutocomplete()) {
+        let entry = interaction.options.getFocussed()
+        const choices = ['Popular Topics: Threads', 'Sharding: Getting started', 'Library: Voice Connections', 'Interactions: Replying to slash commands', 'Popular Topics: Embed preview'];
+		const filtered = choices.filter(choice => choice.startsWith(entry));
+		await interaction.respond(
+			filtered.map(choice => ({ name: choice, value: choice })),
+		);
+      }
+  }
+}
