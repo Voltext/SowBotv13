b9f330f0ac993b7b1c4615ea9df7df61286c6ba0
Voltext <fbricchi@esimed.fr>
2023-03-14 17:25:48 +0100

    fix

--- Events/Interaction/autoComplete.js
+++ Events/Interaction/autoComplete.js
@@ -4,7 +4,7 @@ module.exports = {
   name: "interactionCreate",
   async execute(interaction, client) {
       if(interaction.isAutocomplete()) {
-        let entry = interaction.options.getFocussed()
+        let entry = interaction.options.getFocused()
         const choices = ['Popular Topics: Threads', 'Sharding: Getting started', 'Library: Voice Connections', 'Interactions: Replying to slash commands', 'Popular Topics: Embed preview'];
 		const filtered = choices.filter(choice => choice.startsWith(entry));
 		await interaction.respond(
