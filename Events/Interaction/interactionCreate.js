36be4283a16c9ef8bb0bb041b116f61a01dad8c3
Voltext <lokin84250@live.fr>
2023-03-30 14:58:14 +0200

    fix

--- Events/Interaction/interactionCreate.js
+++ Events/Interaction/interactionCreate.js
@@ -5,7 +5,7 @@ const {
 module.exports = {
     name: "interactionCreate",
     async execute(interaction, client) {
-        if (interaction.isCommand() || interaction.isContextMenuCommand()) {
+        if (interaction.isCommand() || interaction.isContextMenu()) {
             const command = client.commands.get(interaction.commandName);
             if (!command) return interaction.reply({
                 embeds: [
